915a438e92105f3da1661c62f5648975c653f44d
Heitor Augusto Melecardi do Amaral <48411053+HeitorAmaral@users.noreply.github.com>
2019-11-27 20:37:43 -0300

    Adjustments and Validations.

--- faculdade/admin.py
+++ faculdade/admin.py
@@ -1,8 +1,36 @@
 from django.contrib import admin
 from .models import Disciplina, Aluno, Professor, Matricula, Alocacao
 
-admin.site.register(Aluno)
-admin.site.register(Disciplina)
-admin.site.register(Professor)
-admin.site.register(Matricula)
-admin.site.register(Alocacao)
+
+@admin.register(Disciplina)
+class admin_disciplina(admin.ModelAdmin):
+    list_display = ['codigo', 'titulo']
+
+
+@admin.register(Aluno)
+class admin_aluno(admin.ModelAdmin):
+    list_display = ['num_mat', 'nome']
+
+
+@admin.register(Professor)
+class admin_professor(admin.ModelAdmin):
+    list_display = ['nfunc', 'nome', 'endereco_cidade', 'endereco_bairro',
+                    'endereco_rua', 'endereco_numero', 'endereco_complemento', 'fone', 'titulacao']
+
+
+@admin.register(Matricula)
+class admin_matricula(admin.ModelAdmin):
+    list_display = ['aluno', 'disciplina', 'ano', 'faltas', 'nota_final']
+
+
+@admin.register(Alocacao)
+class admin_alocacao(admin.ModelAdmin):
+    list_display = ['professor', 'disciplina',
+                    'ano_letivo', 'carga', 'horario']
+
+
+# admin.site.register(Aluno)
+# admin.site.register(Disciplina)
+# admin.site.register(Professor)
+# admin.site.register(Matricula)
+# admin.site.register(Alocacao)
--- faculdade/models.py
+++ faculdade/models.py
@@ -4,7 +4,8 @@ from datetime import datetime
 
 class Aluno(models.Model):
     num_mat = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* Nome do Aluno", help_text="Insira o nome completo do Aluno, com limite de 100 caracteres.")
+    nome = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* Nome do Aluno",
+                            help_text="Insira o nome completo do Aluno, com limite de 100 caracteres.")
 
     def __str__(self):
         return self.nome
@@ -12,7 +13,8 @@ class Aluno(models.Model):
 
 class Disciplina(models.Model):
     codigo = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=50, null=False, blank=False, default="", verbose_name="* Nome da Disciplina", help_text="Insira o nome da Disciplina, com limite de 50 caracteres.")
+    titulo = models.CharField(max_length=50, null=False, blank=False, default="", verbose_name="* Nome da Disciplina",
+                              help_text="Insira o nome da Disciplina, com limite de 50 caracteres.")
 
     def __str__(self):
         return self.titulo
@@ -20,14 +22,22 @@ class Disciplina(models.Model):
 
 class Professor(models.Model):
     nfunc = models.AutoField(primary_key=True)
-    nome = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* Nome do Professor", help_text="Insira o nome completo do Professor, com limite de 100 caracteres.")
-    endereco_cidade = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* (Endereço) Cidade", help_text="Insira a cidade de moradia do Professor, com limite de 100 caracteres.")
-    endereco_bairro = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* (Endereço) Bairro", help_text="Insira o bairro de moradia do Professor, com limite de 100 caracteres.")
-    endereco_rua = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* (Endereço) Rua", help_text="Insira a rua de moradia do Professor, com limite de 100 caracteres.")
-    endereco_numero = models.CharField(max_length=10, null=False, blank=False, default="", verbose_name="* (Endereço) Número", help_text="Insira o número da moradia do Professor, com limite de 10 caracteres.")
-    endereco_complemento = models.CharField(max_length=50, default="Sem complemento", verbose_name=" (Endereço) Complemento", help_text="Insira o complemento da moradia do Professor, com limite de 50 caracteres.")
-    fone = models.CharField(max_length=20, null=False, blank=False, default="(99) 99999-9999", verbose_name="* Telefone do Professor", help_text="Insira o telefone do Professor.")
-    titulacao = models.CharField(max_length=50, null=False, blank=False, default="", verbose_name="* Titulação do Professor", help_text="Insira a titulação do Professor, com limite de 50 caracteres.")
+    nome = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* Nome do Professor",
+                            help_text="Insira o nome completo do Professor, com limite de 100 caracteres.")
+    endereco_cidade = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* (Endereço) Cidade",
+                                       help_text="Insira a cidade de moradia do Professor, com limite de 100 caracteres.")
+    endereco_bairro = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* (Endereço) Bairro",
+                                       help_text="Insira o bairro de moradia do Professor, com limite de 100 caracteres.")
+    endereco_rua = models.CharField(max_length=100, null=False, blank=False, default="", verbose_name="* (Endereço) Rua",
+                                    help_text="Insira a rua de moradia do Professor, com limite de 100 caracteres.")
+    endereco_numero = models.CharField(max_length=10, null=False, blank=False, default="", verbose_name="* (Endereço) Número",
+                                       help_text="Insira o número da moradia do Professor, com limite de 10 caracteres.")
+    endereco_complemento = models.CharField(max_length=50, default="Sem complemento", verbose_name=" (Endereço) Complemento",
+                                            help_text="Insira o complemento da moradia do Professor, com limite de 50 caracteres.")
+    fone = models.CharField(max_length=20, null=False, blank=False, default="(99) 99999-9999",
+                            verbose_name="* Telefone do Professor", help_text="Insira o telefone do Professor.")
+    titulacao = models.CharField(max_length=50, null=False, blank=False, default="", verbose_name="* Titulação do Professor",
+                                 help_text="Insira a titulação do Professor, com limite de 50 caracteres.")
 
     def __str__(self):
         return self.nome
@@ -52,5 +62,4 @@ class Alocacao(models.Model):
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
 
     def __str__(self):
-        retorno = self.professor.nome + ',' + self.disciplina.titulo
-        return retorno
+        return self.professor.nome
